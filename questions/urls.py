43a44f90a8a818becdf815cdaf501243d1c14c09
Juuwe <danya070207@gmail.com>
2026-04-02 21:11:30 +0300

    feat: add questions app

---
+++ questions/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from questions import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('hot/', views.hot, name='hot'),
+    path('tag/<str:tag_name>', views.tag, name='tag'),
+    path('question/<int:question_id>', views.question, name='question'),
+    path('ask/', views.ask, name='ask'),
+]
