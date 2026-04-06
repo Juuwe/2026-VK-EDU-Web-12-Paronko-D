b81f2b977464cb3d890a52e3cc66ceea16cd2b1e
Juuwe <danya070207@gmail.com>
2026-04-06 15:35:17 +0300

    config: fix static paths

--- application/settings.py
+++ application/settings.py
@@ -119,10 +119,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / "static"
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static'
+    BASE_DIR / "static_dev",
 ]
 
 MEDIA_URL = '/media/'
