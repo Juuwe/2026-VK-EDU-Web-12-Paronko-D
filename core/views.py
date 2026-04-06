695e04d29e0c591ca0f206ad3cd2ab0553e2453d
Juuwe <danya070207@gmail.com>
2026-04-06 15:04:52 +0300

    feat: add views for core app

--- core/views.py
+++ core/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+PROFILE = {"rep": 40, "answers": 23, "views": "115k", "questions": 30,
+            "tags": [{"name": f'tag{i}', "count": 3} for i in range(5)]}
+
 def login(request):
     return render(request, 'core/login.html')
 
@@ -7,7 +10,8 @@ def signup(request):
     return render(request, 'core/signup.html')
 
 def profile(request):
-    return render(request, 'core/profile.html')
+    context = {"profile": PROFILE, "top_tags": PROFILE["tags"]}
+    return render(request, 'core/profile.html', context)
 
 def settings(request):
     return render(request, 'core/settings.html')
