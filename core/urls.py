4c28ef32297b94aa1968967a5bde9424021fe324
Juuwe <danya070207@gmail.com>
2026-05-12 16:08:59 +0300

    fix: rewrite views to CBV

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-
+from django.contrib.auth.views import LogoutView
 from core import views
 
 urlpatterns = [
-    path('login/', views.login, name='login'),
-    path('signup/', views.signup, name='signup'),
-    path('profile/', views.profile, name='profile'),
-    path('settings/', views.settings, name='settings')
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('signup/', views.SignupView.as_view(), name='signup'),
+    path('profile/', views.ProfileView.as_view(), name='profile'),
+    path('settings/', views.SettingsView.as_view(), name='settings'),
+    path('logout/', LogoutView.as_view(), name='logout')
 ]
--- core/views.py
+++ core/views.py
@@ -1,38 +1,89 @@
-from django.shortcuts import render
-
+from django.shortcuts import redirect
+from django.contrib.auth import login
+from django.views.generic import FormView, CreateView, UpdateView, DetailView
+from django.urls import reverse, reverse_lazy
 from .forms import LoginForm, SignupForm, SettingsForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.utils.http import url_has_allowed_host_and_scheme
+from django.contrib.auth.models import User
+from .models import Profile
 
 PROFILE = {"rep": 40, "answers": 23, "views": "115k", "questions": 30,
             "tags": [{"name": f'tag{i}', "count": 3} for i in range(5)]}
 
-def login(request):
-    login_form = LoginForm()
-    context = {'form': login_form}
-    return render(request, 'core/login.html', context)
+class LoginView(FormView):
+    form_class = LoginForm
+    template_name = 'core/login.html'
 
-def signup(request):
-    if request.method == 'POST':
-        signup_form = SignupForm(request.POST, request.FILES)
-        if signup_form.is_valid():
-            pass
-    else:
-        signup_form = SignupForm()
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['request'] = self.request
+        return kwargs
 
-    context = {'form': signup_form}
-    return render(request, 'core/signup.html', context)
+    def form_valid(self, form):
+        login(request=self.request, user=form.get_user())
 
-def profile(request):
-    context = {"profile": PROFILE, "top_tags": PROFILE["tags"]}
-    return render(request, 'core/profile.html', context)
+        if not form.cleaned_data.get('remember_me'):
+            self.request.session.set_expiry(0)
 
-def settings(request):
-    if request.method == 'POST':
-        settings_form = SettingsForm(request.POST, request.FILES)
-        if settings_form.is_valid():
-            pass
-    else:
-        settings_form = SettingsForm(initial={'login': 'mocklogin', 'email': 'mock_email@mail.ru', 'nickname': 'mock_nick'})
+        return super().form_valid(form)
 
-    context = {'form': settings_form}
+    def get_success_url(self):
+        next_url = self.request.GET.get('next')
 
-    return render(request, 'core/settings.html', context)
+        is_safe = url_has_allowed_host_and_scheme(
+            url=next_url,
+            allowed_hosts={self.request.get_host()},
+            require_https=self.request.is_secure(),
+        )
+
+        if next_url and is_safe:
+            return next_url
+
+        return reverse('index')
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('index')
+        return super().dispatch(request, *args, **kwargs)
+
+class ProfileView(LoginRequiredMixin, DetailView):
+    model = Profile
+    template_name = 'core/profile.html'
+
+    def get_object(self, queryset=None):
+        return Profile.objects.get_profile_stat(self.request.user)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['top_tags'] = Profile.objects.get_top_tags(self.request.user)
+        return context
+
+class SignupView(CreateView):
+    form_class = SignupForm
+    template_name = 'core/signup.html'
+    success_url = reverse_lazy('login')
+
+class SettingsView(LoginRequiredMixin, UpdateView):
+    model = User
+    form_class = SettingsForm
+    template_name = 'core/settings.html'
+    success_url = reverse_lazy('settings')
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['user'] = self.request.user
+        return kwargs
+
+    def get_initial(self):
+        initial = super().get_initial()
+
+        profile = self.request.user.profile
+
+        initial['nickname'] = profile.nickname
+        initial['avatar'] = profile.avatar
+
+        return initial
