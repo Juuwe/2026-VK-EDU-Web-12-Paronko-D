aec50a66d3d8ee057df18f01f1929b9b8ade3631
Juuwe <danya070207@gmail.com>
2026-05-12 16:12:47 +0300

    update validators

--- questions/validators.py
+++ questions/validators.py
@@ -3,3 +3,7 @@ from django.core.exceptions import ValidationError
 def validate_created_date(create_at_before, created_at_after, message):
     if created_at_after and created_at_after and create_at_before > created_at_after:
         raise ValidationError(message)
+
+def validate_tags_count(tags, message):
+    if tags.count() > 5:
+        raise ValidationError(message)
