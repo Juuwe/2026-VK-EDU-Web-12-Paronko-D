82c7a2b9353f42c931b7acc6d8b4d7de5e07ad2f
Juuwe <danya070207@gmail.com>
2026-05-12 16:13:44 +0300

    fix

--- questions/urls.py
+++ questions/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('hot/', views.HotView.as_view(), name='hot'),
     path('tag/<str:tag_name>/', views.ByTagView.as_view(), name='tag'),
     path('question/<int:pk>/', views.DetailQuestionView.as_view(), name='question'),
-    path('ask/', views.ask, name='ask'),
+    path('ask/', views.AskQuestionView.as_view(), name='ask'),
 ]
