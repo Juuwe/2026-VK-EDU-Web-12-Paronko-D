fe2314ae4401ab680af6bf27cd151e06d9f7a52e
Juuwe <danya070207@gmail.com>
2026-05-14 04:36:20 +0300

    feat: add uploat_to to avatar

--- core/models.py
+++ core/models.py
@@ -1,6 +1,10 @@
 from django.utils import timezone
 from django.db import models
 from .managers import ProfileManager
+from .utils import get_avatar_upload_path
+from django.core.validators import FileExtensionValidator
+from .validators import validate_file_size
+
 
 class Profile(models.Model):
     objects = ProfileManager()
@@ -8,7 +12,7 @@ class Profile(models.Model):
     user = models.OneToOneField("auth.User", verbose_name="Аккаунт", on_delete=models.CASCADE, related_name='profile')
 
     nickname = models.CharField(verbose_name="Никнейм", max_length=50, unique=True)
-    avatar = models.ImageField(verbose_name="Аватар", upload_to="", null=True, blank=True)
+    avatar = models.ImageField(verbose_name="Аватар", upload_to=get_avatar_upload_path, null=True, blank=True, validators=[FileExtensionValidator(allowed_extensions=['jpg', 'png']), validate_file_size])
     created_at = models.DateTimeField(verbose_name="Дата и время создания", default=timezone.now)
 
     correct_answers_count = models.PositiveIntegerField(verbose_name="Кол-во правильных ответов", default=0)
